31288d5077bcfd1435d9c9e7f9cd8f51ac8e7409
batman <180425939+bbbbbbbbbbbbbb121@users.noreply.github.com>
2025-10-19 18:53:04 +0300

    Fixed enter always working

--- ChatBot.lua
+++ ChatBot.lua
@@ -314,7 +314,7 @@ user.InputBegan:Connect(function(Input, GPE)
 
 			box:CaptureFocus()
 		else -- Just send the message
-			if isGenerating then return end
+			if isGenerating or GPE then return end
 
 			local Prompt = box.Text
 			box.Text = ""
